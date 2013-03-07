a87b9d86378694fef2bde344835fde94f2115af9
Abhishek <asharma@thoughtworks.com>
2013-03-07 10:11:59 +0530

    Abs #0 Initializing the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarize::Application.config.secret_token = '20bccdfc3278be0a1d5d57fcd79bca66444a716e7a75e81e8d33866576f9df0c4776e1ad0fe4621f8a9e53891b1cfbdba83cff015eb3d72bedad78b9ad64dca5'
